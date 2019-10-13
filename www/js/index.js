dac8a077c90e6dcabd98014fa5b064b5bff83647
TEMablo <55841326+TEMablo@users.noreply.github.com>
2019-10-13 16:19:26 +0100

    Commented Index.jx
    Index.jx has recieved more detailed commenting to allow for easier understanding of the project as a whole.

--- www/js/index.js
+++ www/js/index.js
@@ -2,9 +2,10 @@ var paused_count =0;
 var resumed_count = 0;
 var launched_count = 0;
 
+// This is where the deviceready event listener is defined. 
 document.addEventListener("deviceready", onDeviceReady, false);
 		
-	
+// This will update the screen with the number of times each lifecycle event has occured. 
 function updateDisplay() {
 	$("#launched").text("Application launched: " + launched_count);
 	$("#resumed").text("Application paused: " + paused_count);
@@ -14,30 +15,42 @@ function updateDisplay() {
 
 // device APIs are available
 //
+    // When Cordova is ready, called by event listener deviceready.
     function onDeviceReady() {
 	
+    // The resume event listener is defined here. This will be triggered when application is retrieved from the background. This calls the onResume function. 
 	document.addEventListener("resume", onResume, false);
+        
+    // The pause event listener is defined here. This is triggered when the application is pushed to the background. This calls the onPause function. 
 	document.addEventListener("pause", onPause, false);
 	
+    // On app launch, increment the counter.
 	launched_count++;
+    // Update the display.
 	updateDisplay();
-	    
+	   
 	alert("device ready");
     }
 
-
+    // When the application is pushed to the background.
     function onPause() {
 	
+    // Increment the pause counter.
 	paused_count++;
+        
+    // Update the display.
 	updateDisplay();
 	    
 	alert("pause");
     }
 	
-
+    // When the application is resumed.
     function onResume() {
-		
+        
+    // Increment the resumed counter.
 	resumed_count++;
+        
+    // Update the display. 
 	updateDisplay();
 	    
 	alert("resume");
